89d16bcd5bff0554d16b72f8f4f10c3a1c4c39d3
Nils Wloka <nils.wloka@codecentric.de>
2015-06-17 10:35:35 +0200

    Update project.clj
    Remove unwanted dependencies
    Add configuration for lein beanstalk

--- project.clj
+++ project.clj
@@ -4,15 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[compojure "1.3.4"]
-                 [com.cemerick/friend "0.2.1"]
                  [hiccup "1.0.5"]
-                 [liberator "0.12.0"]
                  [org.clojure/clojure "1.6.0"]                 
                  [org.clojure/data.json "0.2.6"]
                  [ring/ring "1.4.0-RC1"]]
   :profiles {:dev {:dependencies [[lein-ring "0.9.6"]
-                                  [ring-mock "0.2.0"]]}}
-  :plugins [[lein-ring "0.9.6"]]
+                                  [ring/ring-mock "0.2.0"]]}}
+  :plugins [[lein-ring "0.9.6"]
+            [lein-beanstalk "0.2.7"]]
   :ring {:handler bwertr.web.routes/app
          :auto-reload? true
          :auto-refresh? true?
